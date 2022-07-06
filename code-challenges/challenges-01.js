db0326fecf4133ff070563c35443dfdb78d7e124
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-06 14:18:40 +0300

    solve a problem

--- code-challenges/challenges-01.js
+++ code-challenges/challenges-01.js
@@ -32,14 +32,15 @@ return max;
 // Output: 81
 const sumNums = (arr) => {
   let sum=0;
-  for(let i=0; i < arr.length  ; i++){
+  for(let i=0; i < arr.length ; i++){
     if(typeof(arr[i] ) === 'number' ){
       sum =sum + arr[i];
     }
-  
+
   }
   return sum;
 };
+
 // -------------------------------------------------------------------------------------------------------
 
 module.exports = { findMax, sumNums };
